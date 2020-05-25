80dc26343da7ccc175671fb7cad45807cd132a2b
tveijola <tommi.veijola@gmail.com>
2020-05-25 19:02:21 +0300

    created heroku app

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 morgan.token('content', (req, res) => {
   if (req.method === 'POST') {
@@ -88,7 +89,7 @@ app.post('/api/persons', (req, res) => {
   res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
